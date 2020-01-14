2bd9813cd1bbc597e9b0660a391f1daa0c123175
d.middendorp <D-Middendorp@users.noreply.github.com>
2020-01-14 14:19:21 +0100

    Added RoundKey generation step.

--- src/com/Aes256Testing/Main.java
+++ src/com/Aes256Testing/Main.java
@@ -5,45 +5,152 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        LinkedList<String> rotWord;
-        Block16Bytes testCypherKey = new Block16Bytes();
-        testCypherKey.hexCol1.add("2b");
-        testCypherKey.hexCol1.add("7e");
-        testCypherKey.hexCol1.add("15");
-        testCypherKey.hexCol1.add("16");
+        String[][] testCypherKey = new String[4][44];
+        testCypherKey[0][0] = "2b";
+        testCypherKey[1][0] = "7e";
+        testCypherKey[2][0] = "15";
+        testCypherKey[3][0] = "16";
 
-        testCypherKey.hexCol2.add("28");
-        testCypherKey.hexCol2.add("ae");
-        testCypherKey.hexCol2.add("d2");
-        testCypherKey.hexCol2.add("a6");
+        testCypherKey[0][1] = "28";
+        testCypherKey[1][1] = "ae";
+        testCypherKey[2][1] = "d2";
+        testCypherKey[3][1] = "a6";
 
-        testCypherKey.hexCol3.add("ab");
-        testCypherKey.hexCol3.add("f7");
-        testCypherKey.hexCol3.add("15");
-        testCypherKey.hexCol3.add("88");
+        testCypherKey[0][2] = "ab";
+        testCypherKey[1][2] = "f7";
+        testCypherKey[2][2] = "15";
+        testCypherKey[3][2] = "88";
 
-        testCypherKey.hexCol4.add("09");
-        testCypherKey.hexCol4.add("cf");
-        testCypherKey.hexCol4.add("4f");
-        testCypherKey.hexCol4.add("3c");
+        testCypherKey[0][3] = "09";
+        testCypherKey[1][3] = "cf";
+        testCypherKey[2][3] = "4f";
+        testCypherKey[3][3] = "3c";
 
-        rotWord = new LinkedList<>();
-        ListIterator<String> copyIterator = testCypherKey.hexCol4.listIterator(0);
-        while (copyIterator.hasNext()){
-            rotWord.add(copyIterator.next());
+        String[][] testPlainMessage = new String[4][4];
+        testPlainMessage[0][0] = "19";
+        testPlainMessage[1][0] = "3d";
+        testPlainMessage[2][0] = "e3";
+        testPlainMessage[3][0] = "be";
+
+        testPlainMessage[0][1] = "a0";
+        testPlainMessage[1][1] = "f4";
+        testPlainMessage[2][1] = "e2";
+        testPlainMessage[3][1] = "2b";
+
+        testPlainMessage[0][2] = "9a";
+        testPlainMessage[1][2] = "c6";
+        testPlainMessage[2][2] = "8d";
+        testPlainMessage[3][2] = "2a";
+
+        testPlainMessage[0][3] = "e9";
+        testPlainMessage[1][3] = "f8";
+        testPlainMessage[2][3] = "48";
+        testPlainMessage[3][3] = "08";
+
+        /*System.out.println((char) Integer.parseInt("2b",16));
+        System.out.println((char) Integer.parseInt("7e",16));
+        System.out.println((char) Integer.parseInt("15",16));
+        System.out.println((char) Integer.parseInt("16",16));
+        char ch = 'n';
+        String hex = String.format("%02x", (int) ch);
+        System.out.println(hex);
+        System.out.println("-----------------------");*/
+
+        for (int i = 0; i < 4; i++) {
+            String[] PM1 = getColumn2dArray(testPlainMessage, 0);
+            System.out.println(PM1[0]);
+            System.out.println(PM1[1]);
+            System.out.println(PM1[2]);
+            System.out.println(PM1[3]);
         }
+        //generateKeySchedule(testCypherKey);
 
-        System.out.println("---");
+    }
 
-        rotWord.add(rotWord.poll());
-        ListIterator<String> rwIterator = rotWord.listIterator(0);
-        while (rwIterator.hasNext()){
-            System.out.println(rwIterator.next());
+    public static String[] getColumn2dArray(String[][] arr, int x) {
+        String[] colArr = new String[4];
+        colArr[0] = arr[0][x];
+        colArr[1] = arr[1][x];
+        colArr[2] = arr[2][x];
+        colArr[3] = arr[3][x];
+        return colArr;
+    }
+
+    public static void generateKeySchedule(String[][] testCypherKey){
+        for (int i = 4; i < 44; i++) {
+            String[] previousFour = new String[4];
+            previousFour[0] = testCypherKey[0][i-1];
+            previousFour[1] = testCypherKey[1][i-1];
+            previousFour[2] = testCypherKey[2][i-1];
+            previousFour[3] = testCypherKey[3][i-1];
+
+            /*if (i == 8) {
+                System.out.println("Previousfour 8");
+                System.out.println(previousFour[0]);
+                System.out.println(previousFour[1]);
+                System.out.println(previousFour[2]);
+                System.out.println(previousFour[3]);
+                System.out.println("-----------------------");
+            }*/
+
+
+            if (i % 4 == 0) {
+                previousFour = rotWordTransformation(previousFour);
+                /*if (i == 8) {
+                    System.out.println("After rotWord");
+                    System.out.println(previousFour[0]);
+                    System.out.println(previousFour[1]);
+                    System.out.println(previousFour[2]);
+                    System.out.println(previousFour[3]);
+                    System.out.println("-----------------------");
+                }*/
+                previousFour = subBytesTransformation(previousFour);
+            }
+
+            /*if (i == 8) {
+                System.out.println("After subBytes");
+                System.out.println(previousFour[0]);
+                System.out.println(previousFour[1]);
+                System.out.println(previousFour[2]);
+                System.out.println(previousFour[3]);
+                System.out.println("-----------------------");
+            }*/
+
+            String[] colBack4 = new String[4];
+            colBack4[0] = testCypherKey[0][i-4];
+            colBack4[1] = testCypherKey[1][i-4];
+            colBack4[2] = testCypherKey[2][i-4];
+            colBack4[3] = testCypherKey[3][i-4];
+            previousFour = hexXORTransformation(colBack4, previousFour, i);
+
+            if (i > 0) {
+                System.out.println("After XOR: " + i);
+                System.out.println(previousFour[0]);
+                System.out.println(previousFour[1]);
+                System.out.println(previousFour[2]);
+                System.out.println(previousFour[3]);
+                System.out.println("-----------------------");
+            }
+
+            testCypherKey[0][i] = previousFour[0];
+            testCypherKey[1][i] = previousFour[1];
+            testCypherKey[2][i] = previousFour[2];
+            testCypherKey[3][i] = previousFour[3];
+
         }
+    }
 
-        System.out.println("---");
+    public static String[] rotWordTransformation(String[] arr) {
+        String hold = arr[0];
+        arr[0] = arr[1];
+        arr[1] = arr[2];
+        arr[2] = arr[3];
+        arr[3] = hold;
+        return arr;
+    }
 
-        List<String> SBOX = Arrays.asList(
+    public static String[] subBytesTransformation(String[] arr) {
+        final String[] SBOX = {
                 "63", "7c", "77", "7b", "f2", "6b", "6f", "c5", "30", "01", "67", "2b", "fe", "d7", "ab", "76",
                 "ca", "82", "c9", "7d", "fa", "59", "47", "f0", "ad", "d4", "a2", "af", "9c", "a4", "72", "c0",
                 "b7", "fd", "93", "26", "36", "3f", "f7", "cc", "34", "a5", "e5", "f1", "71", "d8", "31", "15",
@@ -59,36 +166,40 @@ public class Main {
                 "ba", "78", "25", "2e", "1c", "a6", "b4", "c6", "e8", "dd", "74", "1f", "4b", "bd", "8b", "8a",
                 "70", "3e", "b5", "66", "48", "03", "f6", "0e", "61", "35", "57", "b9", "86", "c1", "1d", "9e",
                 "e1", "f8", "98", "11", "69", "d9", "8e", "94", "9b", "1e", "87", "e9", "ce", "55", "28", "df",
-                "8c", "a1", "89", "0d", "bf", "e6", "42", "68", "41", "99", "2d", "0f", "b0", "54", "bb", "16"
-        );
+                "8c", "a1", "89", "0d", "bf", "e6", "42", "68", "41", "99", "2d", "0f", "b0", "54", "bb", "16"};
 
-        for (String hex : rotWord) {
-            //System.out.println(hex);
-            //System.out.println(Integer.parseInt(hex,16));
-            System.out.println(SBOX.get(Integer.parseInt(hex,16)));
+        for (int i = 0; i < arr.length; i++) {
+            int x = Integer.parseInt(arr[i].substring(0,1),16);
+            int y = Integer.parseInt(arr[i].substring(1,2),16);
+            int index = x * 16 + y;
+            arr[i] = SBOX[index];
         }
 
-        /*System.out.println((char) Integer.parseInt("2b",16));
-        System.out.println((char) Integer.parseInt("7e",16));
-        System.out.println((char) Integer.parseInt("15",16));
-        System.out.println((char) Integer.parseInt("16",16));
-        char ch = 'n';
-        String hex = String.format("%02x", (int) ch);
-        System.out.println(hex);*/
+        return arr;
     }
-}
 
-class Block16Bytes {
-    LinkedList<String> hexCol1;
-    LinkedList<String> hexCol2;
-    LinkedList<String> hexCol3;
-    LinkedList<String> hexCol4;
+    public static String[] hexXORTransformation(String[] colBack4 , String[] arr, int x) {
+        final String[] RCON = {"01","00","00","00","02","00","00","00","04","00","00","00","08","00","00","00",
+                               "10","00","00","00","20","00","00","00","40","00","00","00","80","00","00","00",
+                               "1b","00","00","00","36","00","00","00",};
 
-    public Block16Bytes(){
-        hexCol1 = new LinkedList<>();
-        hexCol2 = new LinkedList<>();
-        hexCol3 = new LinkedList<>();
-        hexCol4 = new LinkedList<>();
+        if (x % 4 == 0) {
+            for (int i = 0; i < 4; i++) {
+                int numColBack4 = Integer.parseInt(colBack4[i], 16);
+                int numArr = Integer.parseInt(arr[i], 16);
+                int numRCON = Integer.parseInt(RCON[x - 4 + i], 16);
+                arr[i] = String.format("%02x", numColBack4 ^ numArr ^ numRCON);
+            }
+        } else {
+            for (int i = 0; i < 4; i++) {
+                int numColBack4 = Integer.parseInt(colBack4[i], 16);
+                int numArr = Integer.parseInt(arr[i], 16);
+                arr[i] = String.format("%02x", numColBack4 ^ numArr);
+            }
+        }
+
+        return arr;
     }
+
 
 }
