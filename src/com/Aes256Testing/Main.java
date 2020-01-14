3bddb0d6b78ed91c7e91922a72a6055511d4e291
d.middendorp <D-Middendorp@users.noreply.github.com>
2020-01-14 17:01:11 +0100

    Added ShiftRows step and working on MixColumns.

--- src/com/Aes256Testing/Main.java
+++ src/com/Aes256Testing/Main.java
@@ -56,15 +56,58 @@ public class Main {
         System.out.println(hex);
         System.out.println("-----------------------");*/
 
-        for (int i = 0; i < 4; i++) {
-            String[] PM1 = getColumn2dArray(testPlainMessage, 0);
-            System.out.println(PM1[0]);
-            System.out.println(PM1[1]);
-            System.out.println(PM1[2]);
-            System.out.println(PM1[3]);
-        }
-        //generateKeySchedule(testCypherKey);
 
+
+
+
+        //generateKeySchedule(testCypherKey);
+        messageEncryption(testPlainMessage);
+
+    }
+
+    public static void messageEncryption(String[][] testPlainMessage) {
+        for (int i = 0; i < 4; i++) {
+            String[] colPM = getColumn2dArray(testPlainMessage, i);
+            System.out.println("Before subbytes " + i);
+            System.out.println(colPM[0]);
+            System.out.println(colPM[1]);
+            System.out.println(colPM[2]);
+            System.out.println(colPM[3]);
+            System.out.println("---------------");
+            subBytesTransformation(colPM);
+            System.out.println("After subbytes " + i);
+            System.out.println(colPM[0]);
+            System.out.println(colPM[1]);
+            System.out.println(colPM[2]);
+            System.out.println(colPM[3]);
+            System.out.println("---------------");
+            setColumn2dArray(testPlainMessage, colPM, i);
+        }
+
+        print16Block(testPlainMessage);
+        System.out.println("---------------");
+
+        shiftRows(testPlainMessage);
+
+        System.out.println("After shiftRows");
+        print16Block(testPlainMessage);
+        System.out.println("---------------");
+
+        System.out.println("After mixColumns");
+        mixColumns(testPlainMessage);
+        System.out.println("---------------");
+
+
+    }
+
+    public static void print16Block(String[][] block) {
+        for (int i = 0; i < 4; i++) {
+            String msg = "";
+            for (int j = 0; j < 4; j++) {
+                msg += block[i][j] + " ";
+            }
+            System.out.println(msg);
+        }
     }
 
     public static String[] getColumn2dArray(String[][] arr, int x) {
@@ -76,15 +119,71 @@ public class Main {
         return colArr;
     }
 
-    public static void generateKeySchedule(String[][] testCypherKey){
-        for (int i = 4; i < 44; i++) {
-            String[] previousFour = new String[4];
-            previousFour[0] = testCypherKey[0][i-1];
-            previousFour[1] = testCypherKey[1][i-1];
-            previousFour[2] = testCypherKey[2][i-1];
-            previousFour[3] = testCypherKey[3][i-1];
+    public static void setColumn2dArray(String[][] message, String[] col, int x) {
+        message[0][x] = col[0];
+        message[1][x] = col[1];
+        message[2][x] = col[2];
+        message[3][x] = col[3];
+    }
 
-            /*if (i == 8) {
+    public static void shiftRows(String[][] message) {
+        String[] hold = new String [4];
+        for (int i = 1; i < 4; i++) {
+            hold[0] = message[i][0];
+            hold[1] = message[i][1];
+            hold[2] = message[i][2];
+            hold[3] = message[i][3];
+
+            for (int j = 0; j < 4; j++) {
+                if (j-i < 0) {
+                    message[i][4-i+j] = hold[j];
+                } else {
+                    message[i][j-i] = hold[j];
+                }
+            }
+        }
+    }
+
+    public static void mixColumns(String[][] message) {
+        final String[] RGF = {"02","03","01","01",
+                               "01","02","03","01",
+                               "01","01","02","03",
+                               "03","01","01","02",};
+        String[] mod = new String[4];
+        //int sum = 0;
+        for (int i = 0; i < 4; i++) {
+            /*System.out.println(Integer.parseInt(message[i][0],16));
+            System.out.println(Integer.parseInt(RGF[i],16));
+            sum ^= Integer.parseInt(message[i][0],16) * Integer.parseInt(RGF[i],16);
+            System.out.println("Sum = " + sum);*/
+            if (RGF[0] == "02") {
+                int holdInt = Integer.parseInt(message[i][0],16);
+                System.out.println(holdInt);
+                System.out.println(holdInt<<1);
+                //System.out.println(String.format("%02x", holdInt));
+                int xorint = Integer.parseInt("1b",16);
+                System.out.println(xorint);
+                System.out.println(holdInt ^ xorint);
+                System.out.println(String.format("%02x", holdInt ^ xorint));
+                System.out.println(Integer.parseInt("b3",16));
+                //System.out.println(String.format("%02x", holdInt));
+                System.exit(1);
+            }
+
+
+        }
+        //System.out.println(String.format("%02x", sum));
+    }
+
+    public static void generateKeySchedule(String[][] cypherKey){
+        for (int i = 4; i < 44; i++) {
+            String[] previousFour = getColumn2dArray(cypherKey, i-1);
+            /*previousFour[0] = cypherKey[0][i-1];
+            previousFour[1] = cypherKey[1][i-1];
+            previousFour[2] = cypherKey[2][i-1];
+            previousFour[3] = cypherKey[3][i-1];*/
+
+            /*if (i > 0) {
                 System.out.println("Previousfour 8");
                 System.out.println(previousFour[0]);
                 System.out.println(previousFour[1]);
@@ -95,7 +194,7 @@ public class Main {
 
 
             if (i % 4 == 0) {
-                previousFour = rotWordTransformation(previousFour);
+                rotWordTransformation(previousFour);
                 /*if (i == 8) {
                     System.out.println("After rotWord");
                     System.out.println(previousFour[0]);
@@ -104,7 +203,7 @@ public class Main {
                     System.out.println(previousFour[3]);
                     System.out.println("-----------------------");
                 }*/
-                previousFour = subBytesTransformation(previousFour);
+                subBytesTransformation(previousFour);
             }
 
             /*if (i == 8) {
@@ -116,12 +215,12 @@ public class Main {
                 System.out.println("-----------------------");
             }*/
 
-            String[] colBack4 = new String[4];
-            colBack4[0] = testCypherKey[0][i-4];
-            colBack4[1] = testCypherKey[1][i-4];
-            colBack4[2] = testCypherKey[2][i-4];
-            colBack4[3] = testCypherKey[3][i-4];
-            previousFour = hexXORTransformation(colBack4, previousFour, i);
+            String[] colBack4 = getColumn2dArray(cypherKey, i - 4);
+            /*colBack4[0] = cypherKey[0][i-4];
+            colBack4[1] = cypherKey[1][i-4];
+            colBack4[2] = cypherKey[2][i-4];
+            colBack4[3] = cypherKey[3][i-4];*/
+            hexXORTransformation(colBack4, previousFour, i);
 
             if (i > 0) {
                 System.out.println("After XOR: " + i);
@@ -132,24 +231,23 @@ public class Main {
                 System.out.println("-----------------------");
             }
 
-            testCypherKey[0][i] = previousFour[0];
-            testCypherKey[1][i] = previousFour[1];
-            testCypherKey[2][i] = previousFour[2];
-            testCypherKey[3][i] = previousFour[3];
+            cypherKey[0][i] = previousFour[0];
+            cypherKey[1][i] = previousFour[1];
+            cypherKey[2][i] = previousFour[2];
+            cypherKey[3][i] = previousFour[3];
 
         }
     }
 
-    public static String[] rotWordTransformation(String[] arr) {
+    public static void rotWordTransformation(String[] arr) {
         String hold = arr[0];
         arr[0] = arr[1];
         arr[1] = arr[2];
         arr[2] = arr[3];
         arr[3] = hold;
-        return arr;
     }
 
-    public static String[] subBytesTransformation(String[] arr) {
+    public static void subBytesTransformation(String[] arr) {
         final String[] SBOX = {
                 "63", "7c", "77", "7b", "f2", "6b", "6f", "c5", "30", "01", "67", "2b", "fe", "d7", "ab", "76",
                 "ca", "82", "c9", "7d", "fa", "59", "47", "f0", "ad", "d4", "a2", "af", "9c", "a4", "72", "c0",
@@ -174,14 +272,12 @@ public class Main {
             int index = x * 16 + y;
             arr[i] = SBOX[index];
         }
-
-        return arr;
     }
 
-    public static String[] hexXORTransformation(String[] colBack4 , String[] arr, int x) {
+    public static void hexXORTransformation(String[] colBack4 , String[] arr, int x) {
         final String[] RCON = {"01","00","00","00","02","00","00","00","04","00","00","00","08","00","00","00",
                                "10","00","00","00","20","00","00","00","40","00","00","00","80","00","00","00",
-                               "1b","00","00","00","36","00","00","00",};
+                               "1b","00","00","00","36","00","00","00"};
 
         if (x % 4 == 0) {
             for (int i = 0; i < 4; i++) {
@@ -197,8 +293,6 @@ public class Main {
                 arr[i] = String.format("%02x", numColBack4 ^ numArr);
             }
         }
-
-        return arr;
     }
 
 
