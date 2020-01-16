e7bc8f19b12553552155fdce3d294fe7a12343de
d.middendorp <D-Middendorp@users.noreply.github.com>
2020-01-16 14:36:04 +0100

    Cleanup

--- src/com/Aes256Testing/Main.java
+++ src/com/Aes256Testing/Main.java
@@ -1,6 +1,23 @@
 package com.Aes256Testing;
 
 public class Main {
+    private static final String[] SBOX = {
+            "63", "7c", "77", "7b", "f2", "6b", "6f", "c5", "30", "01", "67", "2b", "fe", "d7", "ab", "76",
+            "ca", "82", "c9", "7d", "fa", "59", "47", "f0", "ad", "d4", "a2", "af", "9c", "a4", "72", "c0",
+            "b7", "fd", "93", "26", "36", "3f", "f7", "cc", "34", "a5", "e5", "f1", "71", "d8", "31", "15",
+            "04", "c7", "23", "c3", "18", "96", "05", "9a", "07", "12", "80", "e2", "eb", "27", "b2", "75",
+            "09", "83", "2c", "1a", "1b", "6e", "5a", "a0", "52", "3b", "d6", "b3", "29", "e3", "2f", "84",
+            "53", "d1", "00", "ed", "20", "fc", "b1", "5b", "6a", "cb", "be", "39", "4a", "4c", "58", "cf",
+            "d0", "ef", "aa", "fb", "43", "4d", "33", "85", "45", "f9", "02", "7f", "50", "3c", "9f", "a8",
+            "51", "a3", "40", "8f", "92", "9d", "38", "f5", "bc", "b6", "da", "21", "10", "ff", "f3", "d2",
+            "cd", "0c", "13", "ec", "5f", "97", "44", "17", "c4", "a7", "7e", "3d", "64", "5d", "19", "73",
+            "60", "81", "4f", "dc", "22", "2a", "90", "88", "46", "ee", "b8", "14", "de", "5e", "0b", "db",
+            "e0", "32", "3a", "0a", "49", "06", "24", "5c", "c2", "d3", "ac", "62", "91", "95", "e4", "79",
+            "e7", "c8", "37", "6d", "8d", "d5", "4e", "a9", "6c", "56", "f4", "ea", "65", "7a", "ae", "08",
+            "ba", "78", "25", "2e", "1c", "a6", "b4", "c6", "e8", "dd", "74", "1f", "4b", "bd", "8b", "8a",
+            "70", "3e", "b5", "66", "48", "03", "f6", "0e", "61", "35", "57", "b9", "86", "c1", "1d", "9e",
+            "e1", "f8", "98", "11", "69", "d9", "8e", "94", "9b", "1e", "87", "e9", "ce", "55", "28", "df",
+            "8c", "a1", "89", "0d", "bf", "e6", "42", "68", "41", "99", "2d", "0f", "b0", "54", "bb", "16"};
 
     public static void main(String[] args) {
         String[][] testCypherKey = new String[4][44];
@@ -47,11 +64,10 @@ public class Main {
 
         generateKeySchedule(testCypherKey);
         messageEncryption(testPlainMessage,testCypherKey);
-
     }
 
     public static void messageEncryption(String[][] testPlainMessage, String[][] testCypherKey) {
-        for (int i = 0; i < 4; i++) {
+        /*for (int i = 0; i < 4; i++) {
             String[] colPM = getColumn2dArray(testPlainMessage, i);
             System.out.println("Before subbytes " + i);
             System.out.println(colPM[0]);
@@ -67,8 +83,12 @@ public class Main {
             System.out.println(colPM[3]);
             System.out.println("---------------");
             setColumn2dArray(testPlainMessage, colPM, i);
-        }
-
+        }*/
+        System.out.println("Before subbytes:");
+        print16Block(testPlainMessage);
+        System.out.println("---------------");
+        subBytesTransformation(testPlainMessage);
+        System.out.println("After subbytes:");
         print16Block(testPlainMessage);
         System.out.println("---------------");
 
@@ -89,7 +109,7 @@ public class Main {
 
     }
 
-    public static void print16Block(String[][] block) {
+    private static void print16Block(String[][] block) {
         for (int i = 0; i < 4; i++) {
             String msg = "";
             for (int j = 0; j < 4; j++) {
@@ -99,7 +119,7 @@ public class Main {
         }
     }
 
-    public static String[] getColumn2dArray(String[][] arr, int x) {
+    private static String[] getColumn2dArray(String[][] arr, int x) {
         String[] colArr = new String[4];
         colArr[0] = arr[0][x];
         colArr[1] = arr[1][x];
@@ -108,14 +128,14 @@ public class Main {
         return colArr;
     }
 
-    public static void setColumn2dArray(String[][] message, String[] col, int x) {
+    private static void setColumn2dArray(String[][] message, String[] col, int x) {
         message[0][x] = col[0];
         message[1][x] = col[1];
         message[2][x] = col[2];
         message[3][x] = col[3];
     }
 
-    public static void shiftRows(String[][] message) {
+    private static void shiftRows(String[][] message) {
         String[] hold = new String [4];
         for (int i = 1; i < 4; i++) {
             hold[0] = message[i][0];
@@ -133,7 +153,7 @@ public class Main {
         }
     }
 
-    public static void mixColumns(String[][] message) {
+    private static void mixColumns(String[][] message) {
         for (int i = 0; i < 4; i++) {
             String[] multipliedCol = mixColumnsMultiplyEntireColumn(getColumn2dArray(message, i));
             setColumn2dArray(message,multipliedCol,i);
@@ -142,7 +162,7 @@ public class Main {
     }
 
     // Taken from https://github.com/ajaytee/Comp3260
-    public static byte mixColumnsMultiply(byte numRGF, byte messageInput) {
+    private static byte mixColumnsMultiply(byte numRGF, byte messageInput) {
         byte returnValue = 0;
         byte temp;
         while (numRGF != 0) {
@@ -157,7 +177,7 @@ public class Main {
         return returnValue;
     }
 
-    public static String[] mixColumnsMultiplyEntireColumn(String[] messageCol) {
+    private static String[] mixColumnsMultiplyEntireColumn(String[] messageCol) {
         char[] a = new char[4];
         char[] b = new char[4];
         int[] intResult = new int[4];
@@ -186,7 +206,7 @@ public class Main {
 
     }
 
-    public static void addRoundKey(String[][] message, String[][] cypherKey, int round) {
+    private static void addRoundKey(String[][] message, String[][] cypherKey, int round) {
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 int numMessage = Integer.parseInt(message[j][i],16);
@@ -197,7 +217,7 @@ public class Main {
         print16Block(message);
     }
 
-    public static void generateKeySchedule(String[][] cypherKey){
+    private static void generateKeySchedule(String[][] cypherKey){
         for (int i = 4; i < 44; i++) {
             String[] previousFour = getColumn2dArray(cypherKey, i-1);
 
@@ -226,7 +246,7 @@ public class Main {
         }
     }
 
-    public static void rotWordTransformation(String[] arr) {
+    private static void rotWordTransformation(String[] arr) {
         String hold = arr[0];
         arr[0] = arr[1];
         arr[1] = arr[2];
@@ -234,25 +254,7 @@ public class Main {
         arr[3] = hold;
     }
 
-    public static void subBytesTransformation(String[] arr) {
-        final String[] SBOX = {
-                "63", "7c", "77", "7b", "f2", "6b", "6f", "c5", "30", "01", "67", "2b", "fe", "d7", "ab", "76",
-                "ca", "82", "c9", "7d", "fa", "59", "47", "f0", "ad", "d4", "a2", "af", "9c", "a4", "72", "c0",
-                "b7", "fd", "93", "26", "36", "3f", "f7", "cc", "34", "a5", "e5", "f1", "71", "d8", "31", "15",
-                "04", "c7", "23", "c3", "18", "96", "05", "9a", "07", "12", "80", "e2", "eb", "27", "b2", "75",
-                "09", "83", "2c", "1a", "1b", "6e", "5a", "a0", "52", "3b", "d6", "b3", "29", "e3", "2f", "84",
-                "53", "d1", "00", "ed", "20", "fc", "b1", "5b", "6a", "cb", "be", "39", "4a", "4c", "58", "cf",
-                "d0", "ef", "aa", "fb", "43", "4d", "33", "85", "45", "f9", "02", "7f", "50", "3c", "9f", "a8",
-                "51", "a3", "40", "8f", "92", "9d", "38", "f5", "bc", "b6", "da", "21", "10", "ff", "f3", "d2",
-                "cd", "0c", "13", "ec", "5f", "97", "44", "17", "c4", "a7", "7e", "3d", "64", "5d", "19", "73",
-                "60", "81", "4f", "dc", "22", "2a", "90", "88", "46", "ee", "b8", "14", "de", "5e", "0b", "db",
-                "e0", "32", "3a", "0a", "49", "06", "24", "5c", "c2", "d3", "ac", "62", "91", "95", "e4", "79",
-                "e7", "c8", "37", "6d", "8d", "d5", "4e", "a9", "6c", "56", "f4", "ea", "65", "7a", "ae", "08",
-                "ba", "78", "25", "2e", "1c", "a6", "b4", "c6", "e8", "dd", "74", "1f", "4b", "bd", "8b", "8a",
-                "70", "3e", "b5", "66", "48", "03", "f6", "0e", "61", "35", "57", "b9", "86", "c1", "1d", "9e",
-                "e1", "f8", "98", "11", "69", "d9", "8e", "94", "9b", "1e", "87", "e9", "ce", "55", "28", "df",
-                "8c", "a1", "89", "0d", "bf", "e6", "42", "68", "41", "99", "2d", "0f", "b0", "54", "bb", "16"};
-
+    private static void subBytesTransformation(String[] arr) {
         for (int i = 0; i < arr.length; i++) {
             int x = Integer.parseInt(arr[i].substring(0,1),16);
             int y = Integer.parseInt(arr[i].substring(1,2),16);
@@ -261,26 +263,28 @@ public class Main {
         }
     }
 
-    public static void hexXORTransformation(String[] colBack4 , String[] arr, int x) {
-        final String[] RCON = {"01","00","00","00","02","00","00","00","04","00","00","00","08","00","00","00",
-                               "10","00","00","00","20","00","00","00","40","00","00","00","80","00","00","00",
-                               "1b","00","00","00","36","00","00","00"};
-
-        if (x % 4 == 0) {
-            for (int i = 0; i < 4; i++) {
-                int numColBack4 = Integer.parseInt(colBack4[i], 16);
-                int numArr = Integer.parseInt(arr[i], 16);
-                int numRCON = Integer.parseInt(RCON[x - 4 + i], 16);
-                arr[i] = String.format("%02x", numColBack4 ^ numArr ^ numRCON);
-            }
-        } else {
-            for (int i = 0; i < 4; i++) {
-                int numColBack4 = Integer.parseInt(colBack4[i], 16);
-                int numArr = Integer.parseInt(arr[i], 16);
-                arr[i] = String.format("%02x", numColBack4 ^ numArr);
+    private static void subBytesTransformation(String[][] message) {
+        for (int i = 0; i <4; i++) {
+            for (int j = 0; j < 4; j++) {
+                int x = Integer.parseInt(message[j][i].substring(0, 1), 16);
+                int y = Integer.parseInt(message[j][i].substring(1, 2), 16);
+                int index = x * 16 + y;
+                message[j][i] = SBOX[index];
             }
         }
     }
 
+    private static void hexXORTransformation(String[] colBack4 , String[] arr, int x) {
+        final String[] RCON = {"01","00","00","00","02","00","00","00","04","00","00","00","08","00","00","00",
+                               "10","00","00","00","20","00","00","00","40","00","00","00","80","00","00","00",
+                               "1b","00","00","00","36","00","00","00"};
 
+        for (int i = 0; i < 4; i++) {
+            int numColBack4 = Integer.parseInt(colBack4[i], 16);
+            int numArr = Integer.parseInt(arr[i], 16);
+            int numRCON = 0;
+            if (x % 4 == 0) numRCON = Integer.parseInt(RCON[x - 4 + i], 16);
+            arr[i] = String.format("%02x", numColBack4 ^ numArr ^ numRCON);
+        }
+    }
 }
